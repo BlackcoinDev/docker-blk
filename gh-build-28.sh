cb1f504fa60730d158c41882f3fb588eec735878
BlackcoinDev <dev@blackcoin.dev>
2026-07-05 19:49:07 +0200

    build: multiple arch

--- gh-build-28.sh
+++ gh-build-28.sh
@@ -1,54 +1,43 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname $(realpath $0 ))
-moreBuilder=${BASE_DIR}/moreBuilder
-rm -fr ${moreBuilder}/*
 
-export SYSTYPE=x86_64
 export DockerHub=blackcoinorg
 export HUBLAB=github
 export GITNAME=CoinBlack
 export BRANCH=${GIT_CURRENT_BRANCH}
 export TZ=Etc/UTC
 
-# tag names
-base="${DockerHub}/blackcoin-more-28.4.0-base-${SYSTYPE}:${BRANCH}"
-minimal="${DockerHub}/blackcoin-more-minimal-${SYSTYPE}-v28x:${BRANCH}"
-ubuntu="${DockerHub}/blackcoin-more-ubuntu-${SYSTYPE}-v28x:${BRANCH}"
+# tag names (Option A - Unified multi-arch repositories)
+base="${DockerHub}/blackcoin-more-28.4.0-base:${BRANCH}"
+minimal="${DockerHub}/blackcoin-more-minimal-v28x:${BRANCH}"
+ubuntu="${DockerHub}/blackcoin-more-ubuntu-v28x:${BRANCH}"
 
 sed -i "s|master|${BRANCH}|" ${BASE_DIR}/Dockerfile.minbase
 sed -i "s|master|${BRANCH}|" ${BASE_DIR}/Dockerfile.ubase
 sed -i "s|FROM .* as build|FROM ${base} as build|" ${BASE_DIR}/Dockerfile.ubuntu
 
-echo "${GITHUB_ENV} = GITHUB_ENV"
 echo "DockerHub Account: ${DockerHub}"
 echo "Git Account: ${GITNAME}"
-echo ${BRANCH}
-echo ${SYSTYPE}
-echo ${TZ}
+echo "Branch/Tag: ${BRANCH}"
+echo "Base Image: ${base}"
+echo "Ubuntu Image: ${ubuntu}"
+echo "Minimal Image: ${minimal}"
 
-# build
-# minbase (base using ubuntu)
-# ubuntu (package with full ubuntu distro)
-docker build -t ${base} --network=host -f ${BASE_DIR}/Dockerfile.minbase ${BASE_DIR}
+# 1. Build and push multi-arch Base image
+docker buildx build --platform linux/amd64,linux/arm64 -t ${base} --push -f ${BASE_DIR}/Dockerfile.minbase ${BASE_DIR}
 if [ $? -ne 0 ]; then
     echo "Error building base image" && exit 1
 fi
-docker build -t ${ubuntu} --network=host -f ${BASE_DIR}/Dockerfile.ubuntu ${BASE_DIR}
+
+# 2. Build and push multi-arch Ubuntu image
+docker buildx build --platform linux/amd64,linux/arm64 -t ${ubuntu} --push -f ${BASE_DIR}/Dockerfile.ubuntu ${BASE_DIR}
 if [ $? -ne 0 ]; then
     echo "Error building ubuntu image" && exit 1
 fi
-docker image push ${ubuntu}
-if [ $? -ne 0 ]; then
-    echo "Error pushing image" && exit 1
-fi
 
-
-# minimal (only package binaries and scripts)
-docker run -itd  --network=host --name base ${base} bash
-docker cp base:/parts ${moreBuilder}
-cd ${moreBuilder}
-tar -c . | docker import - ${minimal} &&  docker image push ${minimal}
+# 3. Build and push multi-arch Minimal image
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg BASE_IMAGE=${base} -t ${minimal} --push -f ${BASE_DIR}/Dockerfile.minimal ${BASE_DIR}
 if [ $? -ne 0 ]; then
-    echo "Error creating and pushing minimal image" && exit 1
+    echo "Error building minimal image" && exit 1
 fi
